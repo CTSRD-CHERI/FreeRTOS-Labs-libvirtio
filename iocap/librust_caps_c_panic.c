0d62d4568de7c986b67e1b3c32744820d93163d0
Samuel Stark <sws35@cam.ac.uk>
2024-08-19 13:16:56 +0100

    Stop using fwrite for the panic handler because it doesn't work

--- iocap/librust_caps_c_panic.c
+++ iocap/librust_caps_c_panic.c
@@ -5,5 +5,8 @@
  * Define the function librust_caps_c uses to print debug information when it panics.
  */
 uint64_t ccap_panic_write_utf8(const uint8_t *utf8, uint64_t utf_len) {
-    return fwrite(utf8, 1, utf_len, stderr);
+    for (uint64_t i = 0; i < utf_len; i++) {
+        printf("%c", utf8[i]);
+    }
+    return utf_len;
 }
